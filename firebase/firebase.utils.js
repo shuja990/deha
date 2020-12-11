f1bf8ae58b43e53b7569c2c159bc61381ce8bdcf
shuja990 <shujaali1234@gmail.com>
2020-12-11 06:29:35 +0500

    ddd

---
+++ firebase/firebase.utils.js
@@ -0,0 +1,63 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyB690UiW9Oc1nxWAdIfFgyJl7m8jifXuBs",
+    authDomain: "deha-d254a.firebaseapp.com",
+    projectId: "deha-d254a",
+    storageBucket: "deha-d254a.appspot.com",
+    messagingSenderId: "900887077554",
+    appId: "1:900887077554:web:5394e855fd9c7ea8f45fab",
+    measurementId: "G-MWK3QWJF9G"
+}
+export const createUserProfileDocument = async (userAuth,additionalData) => {
+    if(!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    const snapShot = await userRef.get();
+    if(!snapShot.exists){
+        const{displayName,email} = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,email,createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user',error)
+        }
+    }
+    return userRef;
+     
+}
+firebase.initializeApp(config);
+export const addCollectionAndDocuments = async (collectionKey,objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef,obj);
+    });
+    return await batch.commit()
+}
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+        const {title,items} = doc.data();        
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id : doc.id,
+            title,
+            items
+        }
+    });
+    return transformedCollection.reduce((accumulator,collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    },{});
+} 
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt:'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export default firebase;
--- pages/signup.js
+++ pages/signup.js
@@ -2,13 +2,46 @@ import React, { Component } from 'react';
 import Link from 'next/link';
 import Navbar from '../components/layout/Navbar';
 import Footer from '../components/layout/Footer';
-
+import {auth,createUserProfileDocument} from '../firebase/firebase.utils'
+import Router from 'next/router'
 export class index extends Component {
     state={
-        click: false
+        displayName: '',
+        email: '',
+        password : '',
+        address: '',
+        cell: '',
+        restaurant: ''
+    }
+    handleChange = event => {
+        const {name,value} = event.target;
+        this.setState({[name]: value})
+    }
+    handleSubmit = async event => {
+        event.preventDefault();
+        const {displayName,email,password,address,cell,restaurant,userName} = this.state
+        try {
+            const {user} = await auth.createUserWithEmailAndPassword(
+                email,
+                password
+            );
+            await createUserProfileDocument(user,{displayName,address,cell,restaurant,userName});
+            // const router = useRouter()
+            Router.push('/')
+            this.setState({
+                displayName: '',
+                email: '',
+                password : '',
+                address: '',
+                cell: '',
+                restaurant: '',
+                userName: ''
+            })
+        } catch (error) {
+            console.error(error)
+        }
     }
     render() {
-        if(this.state.click===false){
         return (
             <React.Fragment>
                 <Navbar />
@@ -32,35 +65,43 @@ export class index extends Component {
                             <div className="col-lg-6 col-md-12">
                                 <div className="signup-form">
                                     <h3>Create your Account</h3>
-                                    <form>
+                                    <form onSubmit={this.handleSubmit}>
                                         <div className="row">
                                             <div className="col-lg-12">
                                                 <div className="form-group">
-                                                    <input type="text" className="form-control" placeholder="Username" />
+                                                    <input type="text" className="form-control" value={this.state.userName} name="userName" onChange={this.handleChange} placeholder="Username" />
+                                                </div>
+                                            </div>
+                                            <div className="col-lg-12">
+                                                <div className="form-group">
+                                                    <input type="text" className="form-control" value={this.state.displayName} name="displayName" onChange={this.handleChange} placeholder="Name" />
+                                                </div>
+                                            </div>
+                                            <div className="col-lg-12">
+                                                <div className="form-group">
+                                                    <input type="email" className="form-control" value={this.state.email} name="email" onChange={this.handleChange} placeholder="Email" />
                                                 </div>
                                             </div>
 
                                             <div className="col-lg-12">
                                                 <div className="form-group">
-                                                    <input type="email" className="form-control" placeholder="Email" />
-                                                </div>
-                                            </div>
-
-                                            {/* <div className="col-lg-12">
-                                                <div className="form-group">
-                                                    <input type="password" className="form-control" placeholder="Password" />
-                                                </div>
-                                            </div> */}
-
-                                            <div className="col-lg-12">
-                                                <div className="form-group">
-                                                    <input type="text" className="form-control" placeholder="Restaurant" />
+                                                    <input type="password" className="form-control" value={this.state.password} name="password" onChange={this.handleChange} placeholder="Password" />
                                                 </div>
                                             </div>
 
                                             <div className="col-lg-12">
                                                 <div className="form-group">
-                                                    <input type="number" className="form-control" placeholder="Phone Number" />
+                                                    <input type="text" className="form-control" value={this.state.restaurant} name="restaurant" onChange={this.handleChange} placeholder="Restaurant" />
+                                                </div>
+                                            </div>
+                                            <div className="col-lg-12">
+                                                <div className="form-group">
+                                                    <input type="text" className="form-control" value={this.state.address} name="address" onChange={this.handleChange} placeholder="Address" />
+                                                </div>
+                                            </div>
+                                            <div className="col-lg-12">
+                                                <div className="form-group">
+                                                    <input type="number" className="form-control" value={this.state.cell} name="cell" onChange={this.handleChange} placeholder="Cell Number" />
                                                 </div>
                                             </div>
 
@@ -72,7 +113,7 @@ export class index extends Component {
                                             </div>
 
                                             <div className="col-lg-12">
-                                                <button type="submit" onClick={()=>this.setState({click:true})} className="btn btn-primary">Signup Now!</button>
+                                                <button type="submit" className="btn btn-primary">Signup Now!</button>
                                                 <br />
                                                 <span>Already a registered user? <a href="login.html">Login!</a></span>
                                             </div>
@@ -93,57 +134,6 @@ export class index extends Component {
                 <Footer />
             </React.Fragment>
         );
-        }
-        else if(this.state.click===true){
-            
-        return (
-            <React.Fragment>
-                <Navbar />
-                <div className="page-title-area item-bg1">
-                    <div className="container">
-                        <h1>Join</h1>
-                        <ul>
-                            <li>
-                                <Link href="/">
-                                    <a>Home</a>
-                                </Link>
-                            </li>
-                            <li>Signup</li>
-                        </ul>
-                    </div>
-                </div>
-
-                <section className="signup-area ptb-120">
-                    <div className="container">
-                        <div className="row h-100 align-items-center">
-                            <div className="col-lg-6 col-md-12">
-                                <div className="signup-form">
-                                    <h3>Create your Account</h3>
-                                    <form>
-                                        <div className="row">
-                                            <div className="col-lg-12">
-                                                <div className="form-group">
-                                                    <h3>You have successfully joined!</h3>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-
-                            <div className="col-lg-6 col-md-12">
-                                <div className="signup-image">
-                                    <img src={require("../images/marketing1.png")} alt="image" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-
-                <Footer />
-            </React.Fragment>
-        );
-        }
     }
 }
 
