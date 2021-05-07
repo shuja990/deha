271035b158bbb437338a3e3cdc4b1e63b16dcc1b
shuja990 <shujaali1234@gmail.com>
2021-05-07 08:32:56 +0500

    d

--- pages/signup.js
+++ pages/signup.js
@@ -169,6 +169,42 @@ export class index extends Component {
                         </div>
                     </div>
                 </section>
+                <div className="article-content p-4 m-4">
+                    <h3 style={{textAlign:"center"}}><img src={require("../images/restaurant week.png")} width="200px" height="auto"/></h3>
+                    <p>
+                        <h5 style={{color:"green",textDecoration:"underline"}}>SIGN-UP IN FOUR SIMPLE STEPS</h5>
+                        <span style={{fontWeight:"bold",fontSize:"18px"}}>STEP 1: REGISTRATION ONLINE</span>
+                        
+                        <br/>
+                        Visit the Caribbean American Restaurant Association website and complete digital registration
+                        using the ‘Join’ link at <a href="http://www.linkcaranow.org">Linkcaranow/</a>
+                        <br/>
+                        Note: Your registration provides complimentary inclusion to Deh Abroad Village, a free
+                        information portal containing an inventory of businesses for the Caribbean diaspora.
+                        <br/><br/>
+                        <span style={{fontWeight:"bold",fontSize:"18px"}}>STEP 2: DOWNLOAD ‘BICKLE MEALS’ APP</span>
+                       <br/>
+                        
+                        All order transactions for restaurant week will be done through Bickle Meals App. Your restaurant
+                        will be provided a tablet and there are no setup charges.
+                        <br/>
+                        Note: There is a 15% charge per transaction
+                        <br/><br/>
+                        <span style={{fontWeight:"bold",fontSize:"18px"}}> STEP 3: SUBMIT YOUR SPECIAL MENU – <a href="mailto:orders@bicklemeals.com">orders@bicklemeals.com</a></span>
+                       <br/>
+                        Several weeks before the start of Restaurant Week, Caribbean American Restaurant Association
+                        will contact you to obtain the special menu option to advertise. These include:
+                        <br/>
+                        • Weekly specials of specific items<br/>
+                        • prix-fixe menus ($40)<br/>
+                        • Pick your courses, prices, and dining services for the campaign.<br/><br/>
+                        <span style={{fontWeight:"bold",fontSize:"18px"}}> STEP 4: PROMOTE</span>
+                        <br/>
+                        Promote event to customers, staff and the broader community by acknowledging your
+                        participation with flyers, posters, and/or videos provided by Caribbean American Restaurant
+                        Association.
+                    </p>
+                </div>
             <StatusAlert/>
                 <Footer />
             </React.Fragment>
